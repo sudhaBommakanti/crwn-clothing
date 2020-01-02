73773c11e6305a8c16a75c86a7963d9ac9bca6c9
sudhaBommakanti <mailme.sudha.k@gmail.com>
2020-01-02 10:25:34 +0100

    created homepage component and added scss to the homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => (
 
-    <div className="homePage">
+    <div className="homepage">
         <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SNEAKERS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">HATS</h1>
@@ -18,13 +13,25 @@ const HomePage = () => (
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="title">MEN</h1>
+                    <h1 className="title">JACKETS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="title">WOMEN</h1>
+                    <h1 className="title">SNEAKERS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
